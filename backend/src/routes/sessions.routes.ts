5dd6c0bb750c4adad282689f15d153368671b4f6
Yan <yankaiquecosta.yk@gmail.com>
2020-11-08 19:52:16 -0300

    [ADD] JWT

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -5,17 +5,16 @@ const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
     try {
-        const {email, password} = request.body;
-
-       const authenticateUserService = new AuthenticateUserService();
-       const { user } = await authenticateUserService.execute({
+        const { email, password } = request.body;
+        const authenticateUserService = new AuthenticateUserService();
+        const { user, token } = await authenticateUserService.execute({
             email,
             password,
         });
 
         delete user.password;
 
-        return response.json({ user });
+        return response.json({ user, token });
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
--- backend/src/service/AuthenticateUserService.ts
+++ backend/src/service/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -27,7 +29,13 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination');
         }
 
-        return { user };
+        const token = sign({}, 'cacf35edab19cf67469c18d0e5d01f71', {
+            subject: user.id,
+            expiresIn: '1d',
+
+        });
+
+        return { user, token };
     }
 }
 
