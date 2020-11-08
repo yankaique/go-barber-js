fd08b2ce37d39f5a603fa9450dc66d3419a79efa
Yan <yankaiquecosta.yk@gmail.com>
2020-11-08 18:55:36 -0300

    [ADD] Register user

--- backend/src/service/CreateAppointmentService.ts
+++ backend/src/service/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-    provider: string;
+    provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+    public async execute({ provider_id, date }: RequestDTO): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(
             AppointmentsRepository,
         );
@@ -26,7 +26,7 @@ class CreateAppointmentService {
         }
 
         const appointment = appointmentsRepository.create({
-            provider,
+            provider_id,
             date: appointmentDate,
         });
 
