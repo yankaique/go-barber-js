ccd49f9ed20ac76cde43456c4f6a2a84a2ce9438
Yan <yankaiquecosta.yk@gmail.com>
2020-11-08 23:02:14 -0300

    [ADD][EDIT] Create class error

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import AppError from '../errors/AppError';
 import authConfig from '../config/auth';
 
 interface TokenPayload {
@@ -17,7 +18,7 @@ export default function ensureAuthenticated(
     const authHeader = request.headers.authorization;
 
     if (!authHeader) {
-        throw new Error('JWT Token is missing');
+        throw new AppError('JWT Token is missing', 401);
     }
 
     const [, token] = authHeader.split(' ');
@@ -33,6 +34,6 @@ export default function ensureAuthenticated(
 
         return next();
     } catch {
-        throw new Error('Invalid JWT token');
+        throw new AppError('Invalid JWT token', 401);
     }
 }
--- backend/src/service/AuthenticateUserService.ts
+++ backend/src/service/AuthenticateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+
+import AppError from '../errors/AppError';
 import authConfig from '../config/auth';
 import User from '../models/User';
 
@@ -21,13 +23,13 @@ class AuthenticateUserService {
         const user = await userRepository.findOne({ where: { email } });
 
         if (!user) {
-            throw new Error('Incorrect email/password combination');
+            throw new AppError('Incorrect email/password combination', 401);
         }
 
         const passwordMatched = await compare(password, user.password);
 
         if (!passwordMatched) {
-            throw new Error('Incorrect email/password combination');
+            throw new AppError('Incorrect email/password combination', 401);
         }
 
         const { secret, expiresIn } = authConfig.jwt;
