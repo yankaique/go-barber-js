fc43fcf9fa6c8274bd2992994a564bd89bbb9c75
Yan <yankaiquecosta.yk@gmail.com>
2020-11-08 21:54:40 -0300

    [ADD] Upload Image

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../service/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -21,6 +26,14 @@ usersRouter.post('/', async (request, response) => {
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
+});
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    (request, response) => {
+        return response.json({ ok: true });
 });
 
 export default usersRouter;
