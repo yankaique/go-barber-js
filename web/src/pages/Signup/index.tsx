c5d13ba519db009343462e754d1d94adb2266b02
Yan <yankaiquecosta.yk@gmail.com>
2020-11-17 20:46:59 -0300

    [ADD] Unform added

--- web/src/pages/Signup/index.tsx
+++ web/src/pages/Signup/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -7,30 +8,36 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/input';
 import Button from '../../components/button';
 
-const Signup: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="Gobarber logo" />
+const Signup: React.FC = () => {
+  function handleSubmit(data: Object): void {
+    console.log(data);
+  }
 
-      <form>
-        <h1>Faça seu cadastro</h1>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          placeholder="Senha"
-          type="password"
-        />
-        <Button type="submit">Cadastrar</Button>
-      </form>
-      <a href="make">
-        <FiArrowLeft size={20} />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="Gobarber logo" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="Senha"
+            type="password"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="make">
+          <FiArrowLeft size={20} />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default Signup;
