4d26bd58de1d0ef2d5a97ed0850f4156e605127f
Yan <yankaiquecosta.yk@gmail.com>
2020-11-17 23:03:43 -0300

    [EDIT] Validate errors with Yup

--- web/src/pages/Signup/index.tsx
+++ web/src/pages/Signup/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -9,9 +10,23 @@ import Input from '../../components/input';
 import Button from '../../components/button';
 
 const Signup: React.FC = () => {
-  function handleSubmit(data: Object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: Object): void => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'Senha deve conter no mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
