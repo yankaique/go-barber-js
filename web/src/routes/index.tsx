dd8eebe2ce204de8c9575042f584a4f09c40fdd0
Yan <yankaiquecosta.yk@gmail.com>
2020-11-20 18:45:12 -0300

    [ADD] [EDIT] Authenticating routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './routes';
 
 import Signin from '../pages/Signin/index';
 import Signup from '../pages/Signup/index';
+import Dashboard from '../pages/Dashboard/index';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Signin} />
     <Route path="/signup" exact component={Signup} />
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
