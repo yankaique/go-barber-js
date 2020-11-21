573b36e651da595457e475c38779f3bd597e17b8
Yan <yankaiquecosta.yk@gmail.com>
2020-11-21 19:01:48 -0300

    [ADD] Import image icon

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { Container } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () =>{
     return(
-        <Container />
+        <Container>
+            <Image source={logoImg}/>
+        </Container>
 )}
 
 export default SignIn;
