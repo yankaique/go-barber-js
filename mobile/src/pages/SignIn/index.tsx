167d6c03758e68a04755eecc645ed174d81b9269
Yan <yankaiquecosta.yk@gmail.com>
2020-11-22 18:27:37 -0300

    [ADD] Input and button style

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
+import Input from '../../components/input';
+import Button from '../../components/button';
+
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () =>{
     return(
         <Container>
             <Image source={logoImg}/>
             <Title>Faça seu logon</Title>
+
+            <Input name='mail' icon='mail' placeholder='E-mail' />
+            <Input name='password' icon='lock' placeholder='Senha'/>
+
+            <Button onPress={()=>{console.log('Foi')}}>
+                Entrar
+            </Button>
         </Container>
 )}
 
