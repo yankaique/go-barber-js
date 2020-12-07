4450b3b27b6f69429b436e25273118b62ca5e9e7
Yan <yankaiquecosta.yk@gmail.com>
2020-12-07 01:02:23 -0300

    chore: route controllers

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController'
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -15,20 +15,6 @@ appointmentsRouter.use(ensureAuthenticated);
 //     return response.json(appointment);
 // });
 
-appointmentsRouter.post('/', async (request, response) => {
-    // eslint-disable-next-line camelcase
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = container.resolve(CreateAppointmentService);
-
-    const appointment = await createAppointment.execute({
-        provider_id,
-        date: parsedDate,
-    });
-
-    return response.json(appointment);
-});
+appointmentsRouter.post('/',appointmentsController.create);
 
 export default appointmentsRouter;
--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,47 +1,26 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { container } from "tsyringe";
 
-import CreateUserService from '@modules/users/services/CreateUserService';
+
+import UsersController from '@modules/users/infra/http/controllers/UsersController';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
-import uploadConfig from '../../../../../config/upload';
+import uploadConfig from '@config/upload';
+
+const usersConstroller = new UsersController();
+const userAvatarConstroller = new UserAvatarController();
+
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-    const { name, email, password } = request.body;
-
-    const createUser = container.resolve(CreateUserService);
-
-    const user = await createUser.execute({
-        name,
-        email,
-        password,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-});
+usersRouter.post('/', usersConstroller.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename,
-        });
-
-        delete user.password;
-
-        return response.json(user);
-    },
-);
+    userAvatarConstroller.update);
 
 export default usersRouter;
