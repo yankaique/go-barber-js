24fe86d770f3c948fa0d674f6a2a19946f2a3b05
Yan <yankaiquecosta.yk@gmail.com>
2020-12-08 07:29:13 -0300

    test: create a first test wwith fake repositorie

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,21 @@
-test('sum two numbers',()=>{
-    expect(1 + 2).toBe(3);
-})
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+describe('CreateAppointment',()=>{
+    it('should be able to create a new appointment',async ()=>{
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointmentService = new CreateAppointmentService(fakeAppointmentsRepository)
+
+        const appointment = await createAppointmentService.execute({
+            date: new Date(),
+            provider_id: '123456'
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('123456');
+    })
+
+    // it('should not be able to create two appointments on the same time',()=>{
+    //     expect(1+2).toBe(3)
+    // })
+});
