3f759b2b644ef425017b49ec560ba3fb88eb040a
Yan <yankaiquecosta.yk@gmail.com>
2020-12-15 21:37:54 -0300

    test: unknown user test

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -27,4 +27,35 @@ describe('AuthenticateUser',()=>{
 
     });
 
+    it('should be able to authenticate with non existing user',async ()=>{
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
+
+        expect(authenticateUser.execute({
+            email: 'johndoe@gmail.com',
+            password: '123456'
+        })).rejects.toBeInstanceOf(AppError);
+
+    });
+
+    it('should not be able to authenticate with wrong password',async ()=>{
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+        const createUser = new CreateUserService(fakeUsersRepository,fakeHashProvider);
+        const authenticateUser = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider)
+
+        await createUser.execute({
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            password: '123456'
+        })
+
+        expect(authenticateUser.execute({
+            email: 'johndoe@gmail.com',
+            password: 'wrong-password'
+        })).rejects.toBeInstanceOf(AppError);
+
+    });
+
 });
