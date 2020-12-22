443294ba3096d4f0a80c5d91fa710d37ddb4380d
Yan <yankaiquecosta.yk@gmail.com>
2020-12-22 00:36:26 -0300

    feat: development fake email

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -4,14 +4,15 @@ import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider
 )
 
-// container.registerSingleton<IMailProvider>(
-//     'StorageProvider',
-//     DiskStorageProvider
-// )
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    new EtherealMailProvider()
+)
 
