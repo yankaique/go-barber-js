20ad4e3e5ed990ad29ab5c5594a25697b60ffd1d
Yan <yankaiquecosta.yk@gmail.com>
2020-12-23 17:05:42 -0300

    feat: created a feature to find all month availability

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,8 +1,10 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Raw } from 'typeorm';
+
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 class AppointmentsRepository implements IAppointmentsRepository{
 
@@ -19,6 +21,22 @@ class AppointmentsRepository implements IAppointmentsRepository{
         return findAppointment;
     }
 
+    public async findAllInMonthFromProvider({provider_id, month, year}: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+        const parsedMonth = String(month).padStart(2, '0');
+
+        const appointments = await this.ormRepository.find({
+            where:{
+                provider_id,
+                date: Raw(dateFieldName =>
+                    `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`
+                )
+            }
+        });
+
+        return appointments;
+    }
+
+
     public async create({provider_id, date}: ICreateAppointmentDTO): Promise<Appointment>{
         const appointment = this.ormRepository.create({
             provider_id, date
@@ -28,6 +46,7 @@ class AppointmentsRepository implements IAppointmentsRepository{
 
         return appointment;
     }
+
 }
 
 export default AppointmentsRepository;
---
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,37 @@
+/* eslint-disable camelcase */
+import { injectable, inject } from 'tsyringe';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+    provider_id: string;
+    month: number;
+    year: number;
+}
+
+type IResponse = Array<{
+    day: number;
+    available: boolean;
+}>
+
+@injectable()
+class ListProvidersMonthAvailabilityService {
+
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository : IAppointmentsRepository
+    ){}
+
+    public async execute({ provider_id, month, year }: IRequest): Promise<IResponse> {
+        const appointments = await this.appointmentsRepository.findAllInMonthFromProvider({
+            provider_id,
+            year,
+            month
+        });
+
+        console.log(appointments)
+
+        return [{day: 2, available: false}]
+    }
+}
+
+export default ListProvidersMonthAvailabilityService;
