8a83be37cb97c421b6777820a3e19c655326e835
Yan <yankaiquecosta.yk@gmail.com>
2020-12-26 18:18:05 -0300

    feat: availability controller and routes

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -9,12 +9,6 @@ const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//     const appointment = await appointmentsRepository.find();
-
-//     return response.json(appointment);
-// });
-
 appointmentsRouter.post('/',appointmentsController.create);
 
 export default appointmentsRouter;
--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController'
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController'
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController'
+
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
-const providersRouter = Router();
 const appointmentsController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+
+const providersRouter = Router();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/',appointmentsController.index);
+
+providersRouter.get('/:provider_id/day-availability',providerDayAvailabilityController.index);
+
+providersRouter.get('/:provider_id/month-availability',providerMonthAvailabilityController.index);
+
 
 export default providersRouter;
