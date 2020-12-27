6cd351d4094e09b69a3085717cf0bbf2baf57e13
Yan <yankaiquecosta.yk@gmail.com>
2020-12-27 15:46:04 -0300

    feat: scheduling provider appointment

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,13 +2,17 @@ import { Router } from 'express';
 
 
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController'
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController'
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointments = new ProviderAppointmentsController();
+
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/',appointmentsController.create);
+appointmentsRouter.get('/me',providerAppointments.index);
 
 export default appointmentsRouter;
