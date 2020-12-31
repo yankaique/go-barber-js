b05ab17359ef88a52255253a5f0b954e3e2f75f0
Yan <yankaiquecosta.yk@gmail.com>
2020-12-31 12:26:32 -0300

    feature:  new cache functionality

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IHashProvider from '@modules/users/providers/hashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import AppError from '@shared/errors/AppError';
 
 interface IRequest {
@@ -18,7 +19,10 @@ class CreateUserService {
         private usersRepository: IUsersRepository,
 
         @inject('HashProvider')
-        private hashProvider: IHashProvider
+        private hashProvider: IHashProvider,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ){
 
     }
@@ -39,6 +43,8 @@ class CreateUserService {
             email,
             password: hashedPassword,
         });
+
+        await this.cacheProvider.invalidatePrefix('providers-list');
 
         return user;
     }
--- backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -4,30 +4,42 @@ import cacheConfig from '@config/cache';
 import ICacheProvider from '../models/ICacheProvider';
 
 export default class RedisCacheProvider implements ICacheProvider {
-    private cliente: RedisClient;
+    private client: RedisClient;
 
     constructor(){
-        this.cliente = new Redis(cacheConfig.config.redis);
+        this.client = new Redis(cacheConfig.config.redis);
     }
 
     public async save(key: string, value: any): Promise<void>{
-       await this.cliente.set(key, JSON.stringify(value));
+       await this.client.set(key, JSON.stringify(value));
     }
 
     public async recover<T>(key: string): Promise<T | null>{
-        const data = await this.cliente.get(key);
+        const data = await this.client.get(key);
 
         if(!data){
             return null
         }
 
         const parsedData =  JSON.parse(data) as T;
-
+        console.log(parsedData)
         return parsedData;
     }
 
     public async invalidate(key: string): Promise<void>{
 
+    }
+
+    public async invalidatePrefix(prefix: string): Promise<void>{
+        const keys = await this.client.keys(`${prefix}:*`);
+
+        const pipeline = this.client.pipeline();
+
+        keys.forEach(key => {
+            pipeline.del(key);
+        });
+
+        await pipeline.exec();
     }
 
 }
