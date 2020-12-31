dccebaa1d3e806b4e85728632d56c80781c5a4a9
Yan <yankaiquecosta.yk@gmail.com>
2020-12-31 11:38:02 -0300

    feat: cache providers list

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import { classToClass } from 'class-transformer';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
     user_id: string;
@@ -15,14 +16,25 @@ class ListProvidersService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUserRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ){
 
     }
 
     public async execute({ user_id }: IRequest): Promise<User[]> {
-        const users = await this.usersRepository.findAllProvider({
-            except_user_id: user_id
-        });
+        let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
+
+
+        if(!users){
+            users = await this.usersRepository.findAllProvider({
+                except_user_id: user_id
+            });
+
+            console.log('A query no banco foi feita')
+            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+        }
 
         return classToClass(users);
     }
