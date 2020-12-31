3fa0109670c59b0c816320333e878b68c6f11083
Yan <yankaiquecosta.yk@gmail.com>
2020-12-31 11:12:41 -0300

    feat: create and config cache

--- backend/src/modules/appointments/services/ListProvidersAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProvidersAppointmentsService.ts
@@ -4,6 +4,7 @@ import Appointment from '../infra/typeorm/entities/Appointment';
 // import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
     provider_id: string;
@@ -17,16 +18,25 @@ class ListProvidersAppointmentsService {
 
     constructor(
         @inject('AppointmentsRepository')
-        private appointmentsRepository : IAppointmentsRepository
+        private appointmentsRepository : IAppointmentsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider : ICacheProvider
     ){}
 
     public async execute({ provider_id, day, month, year }: IRequest): Promise<Appointment[]> {
+        const cacheData = await this.cacheProvider.recover('sasdas');
+
+        console.log(cacheData);
+
         const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
             provider_id,
             day,
             month,
             year
         });
+
+        // await this.cacheProvider.save('sasdas', 'sdasd');
 
         return appointments;
     }
