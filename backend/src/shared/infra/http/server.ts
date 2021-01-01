7a2d93103917683327874ec289194598bc0c4bed
Yan <yankaiquecosta.yk@gmail.com>
2021-01-01 17:21:31 -0300

    feat: express rate limit

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,19 +1,22 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import 'express-async-errors';
 
 import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
 import { errors } from 'celebrate';
-import 'express-async-errors';
+
+import cors from 'cors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from '@shared/infra/http/routes/index';
+import rateLimiter from '../middlewares/RateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container'
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
