4fee6cd4b8f546d52db6332b285a853803385148
Yan <yankaiquecosta.yk@gmail.com>
2021-01-03 16:34:57 -0300

    feat: added eager loading

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -47,7 +47,8 @@ class AppointmentsRepository implements IAppointmentsRepository{
                 date: Raw(dateFieldName =>
                     `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`
                 )
-            }
+            },
+            relations: ['user'],
         });
 
         return appointments;
