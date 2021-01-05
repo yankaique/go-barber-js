452102f8bf0988547aa2218ae402c910b323e05c
Yan <yankaiquecosta.yk@gmail.com>
2021-01-05 07:39:19 -0300

    feat: next appointment page

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiLock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 import { useAuth } from '../../hooks/auth';
 
 import logoimg from '../../assets/logo.svg';
@@ -25,6 +34,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/49956758?s=460&u=01a3b5fdd9df94d2e8453e4956aa037bd96a6ec7&v=4"
+                alt="Yan Kaique"
+              />
+              <strong>Yan Kaique</strong>
+              <span>
+                <FiLock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
