5e916de54710a188b00a41ddc55613c140811db4
Yan <yankaiquecosta.yk@gmail.com>
2021-01-15 22:20:55 -0300

    feat: create initial user routes

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Dashboard from '../pages/Dashboard/index';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
+
 
 const App = createStackNavigator();
 
@@ -9,6 +14,10 @@ const AppRoutes: React.FC = () =>(
         screenOptions={{headerShown:false, cardStyle:{backgroundColor:'#312e38'}}}
     >
         <App.Screen name="Dashboard" component={Dashboard}/>
+        <App.Screen name="CreateAppointment" component={CreateAppointment}/>
+        <App.Screen name="AppointmentCreated" component={AppointmentCreated}/>
+
+        <App.Screen name="Profile" component={Profile}/>
     </App.Navigator>
 )
 
