5b338cda6e2b4490974c3bae37beb8d85b07af9b
Yan <yankaiquecosta.yk@gmail.com>
2021-01-17 20:51:41 -0300

    feat: create appointment structure

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, BackButton, HeaderTitle, UserAvatar } from './styles';
+
+interface RouteParams {
+    providerId: string;
+}
 
 const CreateAppointment: React.FC = () =>{
+    const route = useRoute();
+    const { providerId } = route.params as RouteParams;
+    const { user } = useAuth();
+    const { goBack } = useNavigation();
+
+    const navigateBack = useCallback(()=>{
+        goBack();
+    },[goBack]);
 
     return(
-        <View >
-        </View>
+        <Container >
+            <Header>
+                <BackButton onPress={navigateBack}>
+                    <Icon name="chevron-left" size={24} color="#999591" />
+                </BackButton>
+                <HeaderTitle>
+                    Cabeleireiros
+                </HeaderTitle>
+                <UserAvatar source={{uri: user.avatar_url}}/>
+            </Header>
+        </Container>
     )
 }
 
