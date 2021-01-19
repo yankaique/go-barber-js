e2465d140854cb11f104447d550d2186df55771f
Yan <yankaiquecosta.yk@gmail.com>
2021-01-18 22:16:40 -0300

    feat: horizontal provider list structure

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,24 +1,60 @@
-import React, { useCallback } from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Feather';
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, BackButton, HeaderTitle, UserAvatar } from './styles';
+import api from '../../services/api';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+    Container,
+    Header,
+    BackButton,
+    HeaderTitle,
+    UserAvatar,
+    ProvidersListContainer,
+    ProvidersList,
+    ProviderContainer,
+    ProviderAvatar,
+    ProviderName
+} from './styles';
 
 interface RouteParams {
     providerId: string;
 }
 
+export interface Provider {
+    id: string;
+    name: string;
+    avatar_url: string
+}
+
 const CreateAppointment: React.FC = () =>{
+
     const route = useRoute();
-    const { providerId } = route.params as RouteParams;
+    const routeParams = route.params as RouteParams;
+
     const { user } = useAuth();
     const { goBack } = useNavigation();
+
+    const [ providers, setProviders ] = useState<Provider[]>([]);
+    const [ selectedProvider, setSelectedProvider ] = useState(routeParams.providerId);
+
+
+    useEffect(()=>{
+        api.get('/providers')
+        .then(response => {
+            setProviders(response.data)
+            console.log('1 '+providers)
+        });
+    },[]);
 
     const navigateBack = useCallback(()=>{
         goBack();
     },[goBack]);
+
+    const handleSelectProvider = useCallback((providerId: string)=>{
+        setSelectedProvider(providerId);
+    },[setSelectedProvider]);
 
     return(
         <Container >
@@ -31,6 +67,25 @@ const CreateAppointment: React.FC = () =>{
                 </HeaderTitle>
                 <UserAvatar source={{uri: user.avatar_url}}/>
             </Header>
+            <ProvidersListContainer>
+                <ProvidersList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    data={providers}
+                    keyExtractor={(provider:Provider)=> provider.id}
+                    renderItem = {({ item: provider })=>(
+                        <ProviderContainer
+                            onPress={() => handleSelectProvider(provider.id)}
+                            selected={ provider.id === selectedProvider }
+                        >
+                            <ProviderAvatar source={{uri: provider.avatar_url}}/>
+                            <ProviderName
+                                selected={ provider.id === selectedProvider }
+                            >{provider.name}</ProviderName>
+                        </ProviderContainer>
+                    )}
+                />
+            </ProvidersListContainer>
         </Container>
     )
 }
